8b0a62b326a56afb08ba8d320d23d0152f86899a
Vishal <vishal.koosuri1@marist.edu>
2018-03-29 07:01:50 +0000

    Added some functionality for the UI elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name: string = 'Vishal';
+  email: string = 'vishal.koosuri1@gmail.com';
+  phone: number = 2649013;
+  users: string[] = [];
+  isEdit: boolean = false;
+  temp: string;
+  
+  constructor() {
+    console.log("In constructor...");
+    
+  }
+  
+  ngOnInit() {
+    console.log("Ng in it...");
+    
+    this.users = ['Vishal', 'Adarsh'];
+  }
+  
+  toggleEdit(username) {
+    console.log(this.isEdit);
+    this.isEdit = !this.isEdit;
+    console.log("name " +username);
+    
+    this.temp = this.users[username];
+  }
+  
+  updateUser(newuser) {
+    console.log(newuser + " " + this.users.indexOf(newuser));
+    
+    this.users[this.users.indexOf(this.temp)] = newuser;
+  }
 }
